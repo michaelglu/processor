b685d2eff593440e3293519e0835b61245ed3c7e
Januario Carreiro <jjc70@duke.edu>
2020-11-14 17:03:23 -0500

    Updated test3 to count branches

--- bench/test3.s
+++ bench/test3.s
@@ -1,7 +1,7 @@
 addi 	$4, $0, 0 			# a
 addi 	$5, $0, 3			# b
 addi 	$16, $16, 0			# stores result
-addi 	$17, $0, 24			# max b
+addi 	$17, $0, 25			# max b
 addi	$21, $0, 0			# parity
 addi 	$22, $0, 2			# compare
 addi 	$8, $8, 1			# i = i + 1, start of loop
@@ -9,7 +9,7 @@ jal 14
 add 	$10, $3, $0			# remainder
 add 	$16, $16, $10
 addi 	$4, $4, 1			# a = a + 1
-add		$2, $2, 1			# increment branch counter
+addi	$2, $2, 1			# increment branch counter
 blt 	$8, $17, -7			# loop b times
 j 30						# exit program
 addi 	$29, $29, -12		# mod takes two arguments, a and b, and returns result of a mod b
@@ -31,8 +31,10 @@ jr $31
 addi	$23, $23, 1			# j = j + 1
 addi	$21, $21, 1			# p = p + 1
 add		$0, $0, $0			# noop
-blt		$17, $23, 3			# branch to exit
-bne		$21, $22, -5		# branch if p != 2
+addi	$2, $2, 1
+blt		$17, $23, 4			# branch to exit
+addi	$2, $2, 1
+bne		$21, $22, -7		# branch if p != 2
 add		$21, $0, $0			# reset parity
 j 30						# jump if p = 0
 addi 	$1, $0, 1			# exit with code 1 
