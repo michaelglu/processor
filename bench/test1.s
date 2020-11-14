1f6b3e927d1ba0e55e92dc41fb0d163e8e3be986
Januario Carreiro <jjc70@duke.edu>
2020-11-14 03:39:35 -0500

    Updated tests:

--- bench/test1.s
+++ bench/test1.s
@@ -1,38 +1,46 @@
-addi	$16, $0, 0		# reg $16 = 0
-addi	$17, $0, 1		# reg $17 = 1
-addi	$18, $0, 25		# Number of iterations, z
-add		$4, $16, $0		# Agument 1: x ($16)
-add		$5, $17, $0 	# Agument 2: y ($17)
-jal	8					# Save current PC in $ra, and jump to fun
-add		$17, $2, $0		# return value saved in $2. This is y ($17)
-j 36					# jump to exit
-addi 	$29, $29, -12	# Adjust stack pointer, start of function
-sw		$16, 8($29)		# Save $16
-sw		$17, 4($29)		# Save $17
-sw		$18, 0($29)		# Save $18
-add 	$8, $0, $0		# i = 0, counter
-add 	$9, $0, $0		# j = 0, counter
-add 	$10, $0, $0		# k = 0, counter
-addi 	$24, $0, 2		# initialize $24 to 1 for comparison
-addi 	$25, $0, 3		# initialize $25 to 2 for comparison
-addi 	$8, $8, 1		# i = i + 1, start of loop
-addi 	$9, $9, 1		# j = j + 1
-addi 	$10, $10, 1		# k = k + 1
-add 	$11, $5, $0		# temp to store second argument
-add 	$5, $4, $5		# b = a + b
-add 	$4, $11, $0		# old b now first argument of next iteration
-bne 	$9, $24, 2
-add 	$9, $0, $0		# j = 0
-addi 	$20, $20, 1		# keeps track of how many times three_counter occurred
-bne 	$10, $25, 2		# cont_two
-add 	$10, $0, $0		# k = 0
-addi 	$21, $21, 1		# keeps track of how many times two_counter occurred
-lw 		$18, 0($29)		# Load $18, max number of iterations, cont_three
-addi	$19, $19, 1		# counter to check program reached this point
-blt 	$8, $18, -15	# continue iterating until z iterations ae complete
-add 	$2, $5, $0		# Move return value to $2
-lw 		$17, 4($29)		# rstore $17
-lw 		$16, 8($29)		# restore $16
-addi 	$29, $29, 12	# Adjust stack pointer
-j 		$31				# Jump to add stored in $ra
-addi 	$20, $0, 100
+addi	$16, $0, 0			# reg $16 = 0
+addi	$17, $0, 1			# reg $17 = 1
+addi	$18, $0, 125		# Number of iterations, z
+addi	$19, $0, 250		# Number of iterations, m
+add		$4, $16, $0			# Agument 1: x ($16)
+add		$5, $17, $0 		# Agument 2: y ($17)
+jal	9						# Save current PC in $ra, and jump to fun
+add		$17, $3, $0			# return value saved in $3. This is y ($17)
+j 45						# jump to exit
+addi 	$29, $29, -12		# Adjust stack pointer, start of function
+sw		$16, 8($29)			# Save $16
+sw		$17, 4($29)			# Save $17
+sw		$18, 0($29)			# Save $18
+add 	$8, $0, $0			# i = 0, counter
+add 	$9, $0, $0			# j = 0, counter
+add 	$10, $0, $0			# k = 0, counter
+addi 	$24, $0, 2			# initialize $24 to 2 for comparison
+addi 	$25, $0, 3			# initialize $25 to 3 for comparison
+addi 	$8, $8, 1			# i = i + 1, start of loop
+addi 	$9, $9, 1			# j = j + 1
+addi 	$10, $10, 1			# k = k + 1
+add 	$11, $5, $0			# temp to store second argument
+add 	$5, $4, $5			# b = a + b
+add 	$4, $11, $0			# old b now first argument of next iteration
+addi	$2, $2, 1			# increment branch counter by 1
+bne 	$9, $24, 3			# branch to cont_two
+add 	$9, $0, $0			# j = 0, reset counter if $9 = 2
+sub		$5, $5, $4			# b = b - a
+j 30						# jump past cont_two
+addi 	$20, $20, 1			# keeps track of how many times cont_two occurred
+addi	$2, $2, 1			# increment branch counter by 1
+bne 	$10, $25, 3			# branch to cont_three
+add 	$10, $0, $0			# k = 0, reset counter if $10 = 3
+addi	$4, $4, 1			# a = a + 1
+j 36						# jump past cont_three
+addi 	$21, $21, 1			# keeps track of how many times cont_three occurred
+addi	$2, $2, 1			# increment branch counter by 1
+blt 	$8, $18, -20		# continue iterating until z iterations are complete
+blt		$8, $19, -21
+add 	$3, $5, $0			# Move return value to $3
+lw		$18, 0($29)			# restore $18
+lw 		$17, 4($29)			# restore $17
+lw 		$16, 8($29)			# restore $16
+addi 	$29, $29, 12		# Adjust stack pointer
+jr $31						# Jump to address stored in $ra
+addi 	$1, $0, 1			# return with Exit Code 1
--- bench/test2.s
+++ bench/test2.s
@@ -1,61 +1,65 @@
-addi 	$4, $0, 1		# a = 1
-addi 	$5, $0, 2		# b = 2
-addi 	$6, $0, 3		# c = 3
-addi 	$7, $0, 4		# d = 4
-addi 	$11, $0, 15		# m = 15 iterations
-addi 	$13, $13, 20	# n = 20 iterations
-addi 	$29, $29, -16	# Adjust stack pointer, start of function
-sw		$16, 12($29)	# Save $16
-sw		$17, 8($29)		# Save $17
-sw		$18, 4($29)		# Save $18
-sw		$19, 0($29)		# Save $19
-addi 	$12, $12, 1		# i, counter, function takes four arguments: a,b,c,d
+addi 	$4, $0, 1			# a = 1
+addi 	$5, $0, 2			# b = 2
+addi 	$6, $0, 3			# c = 3
+addi 	$7, $0, 4			# d = 4
+addi 	$11, $0, 25			# m = 30 iterations
+addi 	$13, $13, 40		# n = 60 iterations
+addi 	$29, $29, -16		# Adjust stack pointer, start of function
+sw		$16, 12($29)		# Save $16
+sw		$17, 8($29)			# Save $17
+sw		$18, 4($29)			# Save $18
+sw		$19, 0($29)			# Save $19
+addi 	$12, $12, 1			# i, counter, function takes four arguments: a,b,c,d
 add 	$16, $4, $0
 add 	$17, $5, $0
 add 	$18, $6, $0
 add 	$19, $7, $0
-add 	$8, $16, $17	# a + b
-mul 	$9, $8, $18		# (a + b) * c
-addi 	$20, $20, 1		# pad with noop
-addi 	$21, $21, 1		# pad with noop
-addi 	$22, $22, 1		# pad with noop
-sub 	$10, $9, $19	# z = ((a + b) * c) - d
-add 	$4, $10, $0		# a = z
-addi	$7, $4, -1
-lw		$19, 0($29)		# restore $19
-lw		$18, 4($29)		# restore $18
-lw 		$17, 8($29)		# restore $17
-lw 		$16, 12($29)	# restore $16
-addi 	$29, $29, 16	# Adjust stack pointer, end of function
-blt 	$12, $11, -24	# foo repeats until i = m
+add 	$8, $16, $17		# a + b
+mul 	$9, $8, $18			# (a + b) * c
+addi 	$20, $20, 1			# pad with noop
+addi 	$21, $21, 1			# pad with noop
+addi 	$22, $22, 1			# pad with noop
+sub 	$10, $9, $19		# z = ((a + b) * c) - d
+add 	$4, $8, $0			# a = z
+addi	$8, $8, 1
+lw		$19, 0($29)			# restore $19
+lw		$18, 4($29)			# restore $18
+lw 		$17, 8($29)			# restore $17
+lw 		$16, 12($29)		# restore $16
+addi 	$29, $29, 16		# Adjust stack pointer, end of function
+addi	$2, $2, 1			# increment branch counter
+blt 	$12, $11, -25		# foo repeats until i = m
 div 	$4, $4, $8
-addi 	$20, $20, 1		# pad with noop
-addi 	$21, $21, 1		# pad with noop
-addi 	$22, $22, 1		# pad with noop
-blt 	$12, $13, -29	# foo repeats until i = n
-addi 	$4, $0, 1000	# a = 1000
-addi 	$6, $0, 3		# c = 5
-addi 	$7, $0, 8		# d = 8
-jal 48
-add 	$16, $4, $0		# goo
-add 	$17, $17, $0	# beginning of loop
+addi 	$20, $20, 1			# pad with noop
+addi 	$21, $21, 1			# pad with noop
+addi 	$22, $22, 1			# pad with noop
+addi	$2, $2, 1			# increment branch counter
+blt 	$12, $13, -31		# foo repeats until i = n
+addi 	$4, $0, 200000		# a = 200000
+addi 	$6, $0, 3			# c = 5
+addi 	$7, $0, 8			# d = 8
+jal 51
+add 	$16, $4, $0			# goo
+add 	$17, $17, $0		# beginning of loop
 sub 	$18, $0, $5
-mul 	$18, $6, $7
-div 	$16, $16, $7	# a / 2
+addi 	$20, $20, 1			# pad with noop
+div 	$16, $16, $7		# a / 2
 addi	$17, $17, 1
 addi 	$18, $18, -1
-blt 	$11, $16, -7 	# goo repeats until a/2 < m
-j 52
-addi	$5, $0, 10		# b = 10
-addi 	$11, $0, 25		# m = 25
-sub 	$7, $5, $7		# d = b - d
+addi 	$2, $2, 1			# increment branch counter
+blt 	$11, $16, -8 		# goo repeats until a/2 < m
+j 55
+addi	$5, $0, 10			# b = 10
+addi 	$11, $0, 25			# m = 25
+sub 	$7, $5, $7			# d = b - d
 jr $31
 add 	$12, $0, $0
-addi	$13, $0, 20		# counter, i
+addi	$13, $0, 40			# counter, i
 add		$14, $13, $13
 and		$15, $12, $13
 mul		$16, $15, $14
 addi	$13, $13, -1
 addi	$12, $12, 1
-bne		$13, $0, -6
-addi 	$1, $0, 1		# exit with code 1
+addi	$2, $2, 1			# increment branch counter
+bne		$13, $0, -7
+addi 	$1, $0, 1			# exit with code 1
